b25c804dc98ee59ab24f39a14ad71fe4ba0f3967
yhl221 <1727538058@qq.com>
2017-09-09 19:20:18 +0800

    add resources

---
+++ resources.js
@@ -0,0 +1,24 @@
+function charges() {
+    return {
+        workingDay:{
+            items:
+            [
+                {time: "9:00 ~ 12:00", unitPrice: 30},
+                {time: "12:00~18:00",unitPrice:50},
+                {time:"18:00~20:00",unitPrice:80},
+                {time:"20:00~22:00",unitPrice:60}
+            ],
+            discount:0.5
+        },
+        offDay:{
+            items:[
+                {item:"9:00~12:00",unitPrice:40},
+                {items:"12:00~18:00",unitPrice:50},
+                {items:"18:00~22:00",unitPrice:60}
+            ],
+            discount:0.25
+        }
+    }
+}
+
+module.exports=charges;
